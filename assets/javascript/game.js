2586db9368311d480e4cd248e85d436b88f14b04
sarabuckley <sara.buckley@live.com>
2018-10-31 14:18:40 -0700

    Added hover mouse effect and timeout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,53 +21,66 @@
 
   // Functions
 
-      function genTargetNumber() {
-          targetNumber = Math.floor(Math.random() * (120 - 19)) + 19; 
-          $("#number-to-guess").html("<p>Target Number</p>" + targetNumber);
-      }
+        function genTargetNumber() {
+            targetNumber = Math.floor(Math.random() * (120 - 19)) + 19; 
+            $("#targetNumber").html("<p>Target Number</p>" + targetNumber);
+        }
 
-      function resetCrystals () {
-          for (var i = 0; i < images.length; i++) {
-              var imageCrystal = $("<img>");
-              imageCrystal.addClass("crystal-image");
-              imageCrystal.attr("src", images[i]);
-              imageCrystal.attr("crystal-value", (Math.floor(Math.random() * 12) + 1));
-              imageCrystal.attr("height", "100");
-              $("#crystals").append(imageCrystal);
-          }
-      }   
+        function resetCrystals () {
+            for (var i = 0; i < images.length; i++) {
+                var imageCrystal = $("<img>");
+                imageCrystal.addClass("crystal-image");
+                imageCrystal.attr("src", images[i]);
+                imageCrystal.attr("crystal-value", (Math.floor(Math.random() * 12) + 1));
+                imageCrystal.attr("height", "100");
+                $("#crystals").append(imageCrystal);
+            }
+        }   
 
-      function resetGame() {
-           counter = 0;
-           $("#totalScore").html("<p>Total Score</p>" + counter);
-           $("#statusMsg").text("");
-           $("#crystals").empty();
-           genTargetNumber();
-           resetCrystals(); 
-      }  
+        function resetGame() {
+            counter = 0;
+            $("#totalScore").html("<p>Total Score</p>" + counter);
+            $("#statusMsg").text("");
+            $("#crystals").empty();
+            genTargetNumber();
+            resetCrystals(); 
+        }  
 
-      function clickCrystals () {
-          var crystalValue = parseInt($(this).attr("crystal-value"));
-          counter += crystalValue;
-          $("#totalScore").html("<p>Total Score</p>" + counter);
-              
-          if (counter > targetNumber) {
-              numLosses++;
-              $("#statusMsg").html("<p>You lost!!</p>");  
-              $("#numLosses").html("Losses: " + numLosses);
-              setTimeout(resetGame, 3000);       
-          }
+        function clickCrystals () {
+            var crystalValue = parseInt($(this).attr("crystal-value"));
 
-          else if (counter === targetNumber) {
-              numWins++;
-              $("#statusMsg").html("<p>You won!!</p>");
-              $("#numWins").html("Wins: " + numWins);
-              setTimeout(resetGame, 4000);         
-          }
-      }
+            if (counter < targetNumber) {   
+                counter += crystalValue;
+                $("#totalScore").html("<p>Total Score</p>" + counter);
+                
+                    if (counter > targetNumber) {
+                        numLosses++;
+                        $("#statusMsg").html("<p>You lost!!</p>");  
+                        $("#numLosses").html("Losses: " + numLosses);
+                        setTimeout(resetGame, 3000);       
+                    }
+
+                    else if (counter === targetNumber) {
+                        numWins++;
+                        $("#statusMsg").html("<p>You won!!</p>");
+                        $("#numWins").html("Wins: " + numWins);
+                        setTimeout(resetGame, 3000);         
+                    }
+            } 
+        }
+
+        function mouseoverCrystals() {
+            this.className = "crystal-image-mouseOver";
+        }
+
+        function mouseoutCrystals() {
+            this.className = "crystal-image";
+        }
 
   // Click event  (must use document as dynamically creating image elements)
-      $(document).on("click", ".crystal-image", clickCrystals);
+      $(document).on("click", ".crystal-image-mouseOver", clickCrystals);
+      $(document).on("mouseover", ".crystal-image", mouseoverCrystals);
+      $(document).on("mouseout", ".crystal-image-mouseOver", mouseoutCrystals);
         
      
 
